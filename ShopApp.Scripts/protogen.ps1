acba314dfc8a8f604ad1423803732e8fba5c4fd6
glemka <42246414+glemka@users.noreply.github.com>
2020-09-08 21:36:40 +0200

    Refactor

--- ShopApp.Scripts/protogen.ps1
+++ ShopApp.Scripts/protogen.ps1
@@ -2,5 +2,5 @@ $protoc = $PSScriptRoot + '.\protoc-3.13.0-win64\protoc.exe'
 $arguments = "--plugin=protoc-gen-ts=..\client-app\node_modules\.bin\protoc-gen-ts.cmd ";
 $arguments +="--js_out=import_style=commonjs,binary:..\client-app\src\generated\Product ";
 $arguments +="--ts_out=service=grpc-web:..\client-app\src\generated\Product "
-$arguments +="-I ..\ShopApp.Product\src\ShopApp.Product.Api\Protos ..\ShopApp.Product\src\ShopApp.Product.Api\Protos\product.proto"
+$arguments +="-I ..\ShopApp.Shared\src\ShopApp.Shared\Protos ..\ShopApp.Shared\src\ShopApp.Shared\Protos\product.proto"
 Start-Process $protoc -ArgumentList $arguments -NoNewWindow
